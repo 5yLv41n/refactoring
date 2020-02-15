c8d7015ea0b67ff424aee6dec7a9f0ebe3b48d0a
5ylv41n77 <5ylv41n77@gmail.com>
2020-02-15 14:30:24 +0100

    Extract method totalAmountTmp

--- 1.1/example.js
+++ 1.1/example.js
@@ -64,6 +64,15 @@ function totalVolumeCredits() {
 
 }
 
+function totalAmountTmp() {
+    let totalAmount = 0;
+    for (let perf of invoiceJson[0].performances) {
+        totalAmount += amountFor(perf);
+    }
+
+    return totalAmount;
+}
+
 let invoice = '[{"customer": "BigCo","performances": [{  "playID": "hamlet",  "audience": "55"},{"playID": "as-like","audience": "35"},{"playID": "othello","audience": "40"}]}]';
 let plays = '{"hamlet": {"name": "Hamlet","type": "tragedy"},"as-like": {"name": "As You Like It","type": "comedy"},"othello": {"name": "Othello","type": "tragedy"}}';
 
