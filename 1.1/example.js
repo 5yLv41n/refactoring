fe3bb055d5debd764b2df2a1cc29bb2bb696b989
5ylv41n77 <5ylv41n77@gmail.com>
2020-02-15 14:47:27 +0100

    add performances object in statementData and remove invoice argument

--- 1.1/example.js
+++ 1.1/example.js
@@ -1,12 +1,13 @@
 function statement(invoice) {
     const statementData = {};
     statementData.customer = invoice.customer;
-    return renderPlainText(statementData, invoice);
+    statementData.performances = invoice.performances;
+    return renderPlainText(statementData);
 }
 
-function renderPlainText(data, invoice) {
+function renderPlainText(data) {
     let result = `Statement for ${data.customer}\n`;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
         result += ` ${playFor(perf).name} : ${usd(amountFor(perf))} (${perf.audience} seats) \n`;
     }
 
@@ -58,7 +59,7 @@ function renderPlainText(data, invoice) {
 
     function totalVolumeCredits() {
         let result = 0;
-        for (let perf of invoiceJson[0].performances) {
+        for (let perf of data.performances) {
             result += volumeCreditsFor(perf);
         }
 
@@ -68,7 +69,7 @@ function renderPlainText(data, invoice) {
 
     function totalAmount() {
         let result = 0;
-        for (let perf of invoiceJson[0].performances) {
+        for (let perf of data.performances) {
             result += amountFor(perf);
         }
 
