ceac203296c773366fac210b88dbdbe12c881649
5ylv41n77 <5ylv41n77@gmail.com>
2020-02-15 12:42:09 +0100

    Rename return variable

--- 1.1/example.js
+++ 1.1/example.js
@@ -46,11 +46,11 @@ function playFor(aPerformance) {
 }
 
 function volumeCreditsFor(perf) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    let result = 0;
+    result += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type) result += Math.floor(perf.audience / 5);
 
-    return volumeCredits;
+    return result;
 }
 
 let invoice = '[{"customer": "BigCo","performances": [{  "playID": "hamlet",  "audience": "55"},{"playID": "as-like","audience": "35"},{"playID": "othello","audience": "40"}]}]';
