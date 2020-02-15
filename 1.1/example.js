c33a98a2aff6ddce19a429a2f39979cd2e0784f4
5ylv41n77 <5ylv41n77@gmail.com>
2020-02-15 14:13:03 +0100

    break the loop

--- 1.1/example.js
+++ 1.1/example.js
@@ -1,12 +1,15 @@
 function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
         // imprime la ligne de cette commande
         result += ` ${playFor(perf).name} : ${usd(amountFor(perf))} (${perf.audience} seats) \n`;
         totalAmount += amountFor(perf);
+    }
+
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
     }
 
     result += `Amount owed is ${usd(totalAmount)}\n`;
